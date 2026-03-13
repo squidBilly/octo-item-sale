d0daf900e687d6f5c779386220e701cf0f1dd6c1
robfox <jymbojymbolaya@hotmail.com>
2026-03-13 15:03:18 +0100

    dependencies added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.ksp)
+    alias(libs.plugins.kotlin.seraialization)
 }
 
 android {
